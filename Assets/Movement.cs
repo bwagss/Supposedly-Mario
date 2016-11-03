eb4f19148dcade57d0da5f298381748b43d3be6c
bwagss <bmwagiu@gmail.com>
2016-11-02 17:49:51 -0700

    Setup of initial project of Mario.

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public int velocity = 10;
+    Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        //rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+        //if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb2D.AddForce(new Vector2(velocity * Input.GetAxis("Horizontal"),0));
+        }
+
+        
+    }
+    
+
+
+}
